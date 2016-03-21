afa9fe1a22c267371337fa724022f743f32acaea
Alex Coyle <abcoyle@gmail.com>
2016-03-21 15:51:41 +0000

    In library_location(), raise an error if there are no file:// locations in the library

--- tools.py
+++ tools.py
@@ -17,12 +17,17 @@ import subprocess
 import fileops
 
 
+class LibraryLocationError(Exception):
+    """To be raised when a file:// library location could not be found"""
+
+
 # Returns the library location for a file,
 # or the default location if the file is not inside any library location
-# Throws an error if there are no file:// locations in the library
+# Raises an error if there are no file:// locations in the library
 def library_location(files, library_locations):
     file_locations = list(l for l in library_locations if l.startswith('file://'))
-    # TODO file_locations could be empty - if so, throw error here
+    if not file_locations:
+        raise LibraryLocationError('No file:// locations could be found in the library')
     return next((l for l in file_locations if files.location.startswith(l)),
                 file_locations[0])
 
